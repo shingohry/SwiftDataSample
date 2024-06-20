3f233600c488eaeed47fb22b49c77c339f429858
Shingo Hiraya <shingohiraya@gmail.com>
2024-06-20 17:02:58 +0900

    Update SwiftDataSampleApp

--- SwiftDataSample/SwiftDataSampleApp.swift
+++ SwiftDataSample/SwiftDataSampleApp.swift
@@ -10,10 +10,31 @@ import SwiftData
 
 @main
 struct SwiftDataSampleApp: App {
+    var container: ModelContainer = {
+        let modelContainer: ModelContainer
+        do {
+            let fileURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("data.json")
+            print("fileURL:", fileURL)
+            let configuration = JSONStoreConfiguration(
+                name: "",
+                schema: Schema([Task.self]),
+                fileURL: fileURL
+            )
+            modelContainer = try ModelContainer(
+                for: Task.self,
+                configurations: configuration
+            )
+        } catch {
+            fatalError("Failed to create the model container: \(error)")
+        }
+        return modelContainer
+    }()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(for: Task.self)
+        .modelContainer(container)
+//        .modelContainer(for: Task.self)
     }
 }
